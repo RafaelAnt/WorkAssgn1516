cd87222121675466742c5b79fe288a2b28ab092a
Nuno Oliveira <nuno.oliv.94@gmail.com>
2015-11-29 23:51:16 +0000

    Codigo funcional com papi funcional com primeiros testes

---
+++ Codigo/run.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+echo Start test > test.txt
+#Versão original
+
+echo Versao original >>test.txt
+i=0
+echo loop: $i >>test.txt
+./mmult_Orig >> test.txt
+i=$((i+1))
+echo loop: $i >>test.txt
+./mmult_Orig >> test.txt
+i=$((i+1))
+echo loop: $i >>test.txt
+./mmult_Orig >> test.txt
+i=$((i+1))
+echo loop: $i >>test.txt
+./mmult_Orig >> test.txt
+i=$((i+1))
+echo loop: $i >>test.txt
+./mmult_Orig >> test.txt
+i=$((i+1))
+echo loop: $i >>test.txt
+./mmult_Orig >> test.txt
+i=$((i+1))
+echo loop: $i >>test.txt
+./mmult_Orig >> test.txt
+i=$((i+1))
+echo loop: $i >>test.txt
+./mmult_Orig >> test.txt
+i=$((i+1))
+echo loop: $i >>test.txt
+./mmult_Orig >> test.txt
+i=$((i+1))
+echo loop: $i >>test.txt
+./mmult_Orig >> test.txt
+
+
+
+
+echo Versao inversa >>test.txt
+i=0
+echo loop: $i >>test.txt
+./mmult_Inverted >> test.txt
+i=$((i+1))
+echo loop: $i >>test.txt
+./mmult_Inverted >> test.txt
+i=$((i+1))
+echo loop: $i >>test.txt
+./mmult_Inverted >> test.txt
+i=$((i+1))
+echo loop: $i >>test.txt
+./mmult_Inverted >> test.txt
+i=$((i+1))
+echo loop: $i >>test.txt
+./mmult_Inverted >> test.txt
+i=$((i+1))
+echo loop: $i >>test.txt
+./mmult_Inverted >> test.txt
+i=$((i+1))
+echo loop: $i >>test.txt
+./mmult_Inverted >> test.txt
+i=$((i+1))
+echo loop: $i >>test.txt
+./mmult_Inverted >> test.txt
+i=$((i+1))
+echo loop: $i >>test.txt
+./mmult_Inverted >> test.txt
+i=$((i+1))
+echo loop: $i >>test.txt
+./mmult_Inverted >> test.txt
+i=$((i+1))
+
