339b65d65b821be188c6b6d75cf4246297ccea65
RafaelAnt <rafael.uminho@hotmail.com>
2015-12-05 13:52:42 +0000

    Resultados de Testes Adicionados

--- Codigo/run.sh
+++ Codigo/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-fileName="mmult_t_vec"
-echo "Bytes;L1 Misses;L2 Misses;L3 Misses;Total Instructions;RAM Accesses p/ Instruction;Time (s);FLOP;GFLOPS;Operational Intensity;" > $fileName.csv
-./$fileName >> $fileName.csv
+fileName="mmult_matB_only1_BxA"
+echo "Bytes;L1 Misses;L2 Misses;L3 Misses;Total Instructions;RAM Accesses p/ Instruction;Time (s);FLOP;GFLOPS;Operational Intensity;" > ../Resultados/$fileName.csv
+./$fileName >> ../Resultados/$fileName.csv
